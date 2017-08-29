d93f7f93de9540fb8af9d4f2691601be45401b3b
leonawicz <matt.rstat@gmail.com>
2017-08-29 12:31:36 -0600

    Add package info.

--- R/snapverse.R
+++ R/snapverse.R
@@ -1,9 +1,28 @@
-# code modified from tidyverse: https://github.com/tidyverse
-# but slightly more general to handle different SNAPverse package subsets
+#' snapverse: The primary sector package for the SNAPverse.
+#'
+#' The snapverse package loads a subset of individual SNAPverse member packages.
+#'
+#' \describe{
+#'   \item{snapfuns}{functions for working with popular, curated SNAP data from SNAPverse data packages.}
+#'   \item{snapprep}{functions for SNAP source data extraction, manipulation and curation for various projects.}
+#'   \item{alfresco}{functions for working with and analyzing ALFRESCO wildfire model inputs and outputs.}
+#'   \item{snapclim}{SNAP climate data.}
+#'   \item{snapfire}{SNAP fire data.}
+#'   \item{snappoly}{collection of popular polygon shapefile data sets used at SNAP.}
+#'   \item{snapmaps}{collection of popular gridded maps used at SNAP.}
+#'   \item{snapdist}{SNAP spatial distributions examples data set.}
+#'   \item{snapapps}{curated collection of SNAP R Shiny apps loadable via R package.}
+#' }
+#'
+#' @docType package
+#' @name snapverse
+NULL
 
 #' @importFrom magrittr %>%
 #' @importFrom rlang !!
 NULL
+
+# code modified from tidyverse: https://github.com/tidyverse
 
 .onAttach <- function(...) {
   # nolint start

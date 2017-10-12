23c371d8ba6100ff61e4966d0fe645c59f73f159
leonawicz <matt.rstat@gmail.com>
2017-10-12 17:19:15 -0600

    Update unit test.

--- tests/testthat/test-snapverse.R
+++ tests/testthat/test-snapverse.R
@@ -1,6 +1,7 @@
 context("snapverse")
 
-x <- snapverse:::.onAttach("snapverse")
+library(snapverse)
+x <- snapverse:::.onAttach()
 
 test_that("loading and updating run", {
   expect_identical(snapverse_update(), NULL)

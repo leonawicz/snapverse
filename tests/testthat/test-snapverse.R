f74d2f95c854ad7db2649a28912e884ecf453092
leonawicz <matt.rstat@gmail.com>
2017-10-12 17:01:03 -0600

    Testing new test with travis.

--- tests/testthat/test-snapverse.R
+++ tests/testthat/test-snapverse.R
@@ -4,5 +4,5 @@ test_that("loading and updating run", {
   expect_identical(snapverse_update(), NULL)
   x <- library(snapverse)
   y <- library(snapverse)
-  expect_identical(library(x, y))
+  expect_identical(x, y)
 })

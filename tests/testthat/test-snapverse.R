8ce2140695824d81817e54068e45badda3c7fdde
leonawicz <matt.rstat@gmail.com>
2017-10-12 17:12:57 -0600

    Final attempt for package attachment unit test on travis.

--- tests/testthat/test-snapverse.R
+++ tests/testthat/test-snapverse.R
@@ -2,7 +2,5 @@ context("snapverse")
 
 test_that("loading and updating run", {
   expect_identical(snapverse_update(), NULL)
-  x <- library(snapverse)
-  y <- library(snapverse)
-  expect_identical(x, y)
+  expect_identical(snapverse:::.onAttach("snapverse"), NULL)
 })

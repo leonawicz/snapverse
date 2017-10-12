2eb12856055ceb08faf9a7a3dc2e92b32b391f4b
leonawicz <matt.rstat@gmail.com>
2017-10-12 17:16:49 -0600

    Update test.

--- tests/testthat/test-snapverse.R
+++ tests/testthat/test-snapverse.R
@@ -1,6 +1,8 @@
 context("snapverse")
 
+x <- snapverse:::.onAttach("snapverse")
+
 test_that("loading and updating run", {
   expect_identical(snapverse_update(), NULL)
-  expect_identical(snapverse:::.onAttach("snapverse"), NULL)
+  expect_identical(x, NULL)
 })
